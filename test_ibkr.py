6c71e052f8140859f3c815b21b5ea843605e77bb
Federico de Nuñez <federicodenunez@gmail.com>
2025-05-12 22:33:50 -0300

    Update test_ibkr.py

--- test_ibkr.py
+++ test_ibkr.py
@@ -1,11 +1,11 @@
 from ib_async import IB, util
 
-def connect_ib_gateway():
+async def connect_ib_gateway():
     ib = IB()
-    ib.connect('127.0.0.1', 4002, clientId=1)
+    await ib.connect('127.0.0.1', 4002, clientId=1)
     print("IB connected:", ib.isConnected())
-    ib.disconnect()
+    await ib.disconnect()
     print("IB disconnected.")
 
 if __name__ == '__main__':
-    util.run(connect_ib_gateway)
+    util.run(connect_ib_gateway())
